eb967de046116d6c1e3500ee51e4994ee48eab33
inteee1 <131097590+inteee1@users.noreply.github.com>
2025-09-02 16:11:55 +0900

    imwrite

---
+++ python/a03_imwrite.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+def main():
+    imgfile = '/home/ab123/opencvZoo2/data/lenna.bmp'
+    img = cv2.imread(imgfile)
+    cv2.imwrite('/home/ab123/opencvZoo2/data/lenna_copy.png', img)
+    cv2.imwrite('/home/ab123/opencvZoo2/data/lenna_copy2.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+    cv2.imwrite('/home/ab123/opencvZoo2/data/lenna_copy.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 60])
+    
+if __name__ == "__main__":
+    main()
